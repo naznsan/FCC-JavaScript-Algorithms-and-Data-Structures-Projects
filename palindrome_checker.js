ff5cbbb51da533fbcd6d35ec61b07fd79660f4bb
Justin Kim <38806488+naznsan@users.noreply.github.com>
2020-04-16 11:58:57 +1200

    Palindrome checker finished

--- palindrome_checker.js
+++ palindrome_checker.js
@@ -1,7 +1,18 @@
 function palindrome(str) {
-  return true;
+	let inputArr = str.replace(/[^0-9a-z]/gi, "").split("");	// Split input string into array, taking out non-alphanumeric characters
+	let inputReverse = inputArr.slice();
+	inputReverse.reverse();
+
+	//console.log(inputArr);
+	//console.log(inputReverse);
+
+	for (let i = 0; i < inputArr.length; i++) {
+		if (inputArr[i].toLowerCase() != inputReverse[i].toLowerCase()) return false;
+	}
+
+    return true;
 }
 
 
 
-console.log(palindrome("eye"));
+console.log(palindrome("A man, a plan, a canal. Panama"));
